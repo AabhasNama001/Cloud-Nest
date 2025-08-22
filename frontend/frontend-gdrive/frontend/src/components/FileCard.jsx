22c70c97bd441ab00db626377a181147e8606bdc
Aabhas Nama <aabhasa9@gmail.com>
2025-08-22 22:01:09 +0530

    completed

--- frontend/frontend-gdrive/frontend/src/components/FileCard.jsx
+++ frontend/frontend-gdrive/frontend/src/components/FileCard.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 const FileCard = ({ file }) => {
   return (
-    <div className="bg-green-800 overflow-auto p-4 rounded shadow hover:bg-gray-600">
-      <div className="text-4xl mb-2">📄</div>
-      <div className="font-medium">{file.name}</div>
-      <a
-        href={`http://localhost:4000${file.url}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-300 underline mt-2 block hover:text-blue-600 w-fit"
-      >
-        Open / Download
-      </a>
-    </div>
+    <a
+      href={`http://localhost:4000${file.url}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="bg-green-800 p-4 rounded shadow hover:bg-gray-600">
+        <div className="text-4xl mb-2">📄</div>
+        <div className="font-medium truncate w-full max-w-[200px]">
+          {file.name}
+        </div>
+        <span className="text-blue-300 underline mt-2 block hover:text-blue-600 w-fit">
+          Open / Download
+        </span>
+      </div>{" "}
+    </a>
   );
 };
 
